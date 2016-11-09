8df1b0470ea17ce2e44152484ee427f53def7616
Lee Samky <Leesamky@Lees-MacBook-Air.local>
2016-11-09 22:19:48 +0700

    next step to save the teamInfo to a file and read the file back as the program starts

---
+++ allen/offline/getMatchInfo.js
@@ -0,0 +1,111 @@
+var mongoose=require('mongoose')
+var _=require('lodash')
+var request=require('request')
+var cheerio=require('cheerio')
+var iconv=require('iconv-lite')
+var teamInfo=require('./model').teamInfo
+
+var match={
+    'homeInfo':{
+        'league':{
+            'home':[],
+            'away':[],
+            'overall':[]
+        }
+    },
+    'awayInfo':{
+        'league':{
+            'home':[],
+            'away':[],
+            'overall':[]
+        }
+    },
+    'matchInfo':{},
+    'homePastMatches':[],
+    'homeFutureMatches':[],
+    'awayPastMatches':[],
+    'awayFutureMatches':[],
+    'bothMatches':[]
+}
+
+var matchUrl=[ '欧U19',
+    'http://liansai.500.com/team/5524/',
+    'http://liansai.500.com/team/5526/',
+    'http://odds.500.com/fenxi/shuju-626168.shtml',
+    'http://liansai.500.com/zuqiu-4090/',
+    '11-09&nbsp;19:00',
+    '资格赛' ]
+
+function objToSave(obj){
+    "use strict";
+    teamInfo.findOne({'teamId':obj['awayId']})
+        .exec(function(error,result){
+            if(error){console.log(error)}
+            else{
+                obj['away']=result['en_name']
+
+                teamInfo.findOne({'teamId':obj['homeId']})//second query
+                    .exec(function(error,result){
+                        if(error){console.log(error)}
+                        else{
+                            obj['home']=result['en_name']
+                            obj['homeSecondHalf']=obj['homeFullTime']-obj['homeFirstHalf']
+                            obj['awaySecondHalf']=obj['awayFullTime']-obj['awayFirstHalf']
+                            obj['date']=new Date(obj['date'])
+                            obj['matchGoals']=obj['homeFullTime']+obj['awayFullTime']
+                            obj['firstHalfGoals']=obj['homeFirstHalf']+obj['awayFirstHalf']
+                            obj['secondHalfGoals']=obj['homeSecondHalf']+obj['awaySecondHalf']
+                            console.log(obj)
+                        }
+                    })
+            }
+        })
+}
+
+
+function getMatchInfo(match,matchUrl){
+    "use strict";
+    var url=matchUrl[1]+'teamfixture/'
+    var options={
+        url:url,
+        headers:{
+            'User-Agent':"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36"
+        },
+        encoding:null
+    }
+    request(options,function(error,response,body){
+        "use strict";
+        if(!error&&response.statusCode===200){
+            var html=iconv.decode(body,'gb2312')
+            var $=cheerio.load(html,{
+                decodeEntities:false
+            })
+            $('div.ltab_bd table.jTrHover').find('tr').each(function () {
+                var temp = {}
+               if(_.isEmpty($(this).find('td').eq(0).find('a').text())) {
+                   return
+               }
+                temp['match'] = $(this).find('td').eq(0).find('a').text()
+                temp['matchId'] = parseInt($(this).attr('id'))
+                temp['date'] = $(this).find('td.td_time').text()
+                var homeTeam = $(this).find('td.td_lteam').find('a').attr('href').split('/')
+                temp['homeId']=parseInt(homeTeam[4])
+                var awayTeam = $(this).find('td.td_rteam').find('a').attr('href').split('/')
+                temp['awayId']=parseInt(awayTeam[4])
+                var score = _.map(_.words($(this).find('td').eq(3).text()),_.parseInt)
+                temp['homeFullTime']=score[0]
+                temp['awayFullTime']=score[1]
+                temp['homeFirstHalf']=score[2]
+                temp['awayFirstHalf']=score[3]
+                // match['homePastMatches'].push(temp)
+                objToSave(temp)
+            });
+            // console.log(match)
+
+        }else{
+            console.log(error)
+        }
+    })
+}
+
+getMatchInfo(match,matchUrl)
