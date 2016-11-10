a47e4021af99ed8b0eb03fedb2670b55996fdcff
Lee Samky <Leesamky@happy.renaissance.edu.vn>
2016-11-10 15:26:10 +0700

    do the team name in future matches

--- allen/offline/getMatchInfo.js
+++ allen/offline/getMatchInfo.js
@@ -69,9 +69,10 @@ function getMatchInfo(match,matchUrl){
             })
             $('div.ltab_bd table.jTrHover').find('tr').each(function () {
                 var temp = {}
-               if(_.isEmpty($(this).find('td').eq(0).find('a').text())) {
-                   return
-               }
+                var score_temp = _.map(_.words($(this).find('td').eq(3).text()),_.parseInt)
+                if(_.isEmpty($(this).find('td').eq(0).find('a').text())||_.isUndefined(score_temp[2])) {
+                    return
+                }
                 temp['league'] = $(this).find('td').eq(0).find('a').text()
                 var s=$(this).find('td').eq(0).find('a').attr('href').split('/')
                 temp['leagueId']=parseInt((s[s.length-2]).slice(6))
@@ -89,6 +90,75 @@ function getMatchInfo(match,matchUrl){
 
 
                 match['homePastMatches'].push(objToSave(temp))
+            });
+
+            $('#f_table').find('tr').each(function(){
+                var temp={}
+
+                if(_.isEmpty($(this).find('td').eq(0).find('a').text())){
+                    return
+                }
+                temp['league'] = $(this).find('td').eq(0).find('a').text()
+                var s=$(this).find('td').eq(0).find('a').attr('href').split('/')
+                temp['leagueId']=parseInt((s[s.length-2]).slice(6))
+                temp['matchId'] = parseInt($(this).attr('id'))
+
+                temp['date'] = new Date($(this).find('td.td_time').text())
+                var homeTeam = $(this).find('td.td_lteam').find('a').attr('href').split('/')
+                temp['home']=parseInt(homeTeam[4])
+                var awayTeam = $(this).find('td.td_rteam').find('a').attr('href').split('/')
+                temp['away']=parseInt(awayTeam[4])
+
+                match['homeFutureMatches'].push(temp)
+            })
+
+            console.log(match)
+
+
+        }else{
+            console.log(error)
+        }
+    })
+
+
+    var url=matchUrl[2]+'teamfixture/'
+    var options={
+        url:url,
+        headers:{
+            'User-Agent':"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36"
+        },
+        encoding:null
+    }
+    request(options,function(error,response,body){
+        "use strict";
+        if(!error&&response.statusCode===200){
+            var html=iconv.decode(body,'gb2312')
+            var $=cheerio.load(html,{
+                decodeEntities:false
+            })
+            $('div.ltab_bd table.jTrHover').find('tr').each(function () {
+                var temp = {}
+                var score_temp = _.map(_.words($(this).find('td').eq(3).text()),_.parseInt)
+                if(_.isEmpty($(this).find('td').eq(0).find('a').text())||_.isUndefined(score_temp[2])) {
+                    return
+                }
+                temp['league'] = $(this).find('td').eq(0).find('a').text()
+                var s=$(this).find('td').eq(0).find('a').attr('href').split('/')
+                temp['leagueId']=parseInt((s[s.length-2]).slice(6))
+                temp['matchId'] = parseInt($(this).attr('id'))
+                temp['date'] = $(this).find('td.td_time').text()
+                var homeTeam = $(this).find('td.td_lteam').find('a').attr('href').split('/')
+                temp['homeId']=parseInt(homeTeam[4])
+                var awayTeam = $(this).find('td.td_rteam').find('a').attr('href').split('/')
+                temp['awayId']=parseInt(awayTeam[4])
+                var score = _.map(_.words($(this).find('td').eq(3).text()),_.parseInt)
+                temp['homeFullTime']=score[0]
+                temp['awayFullTime']=score[1]
+                temp['homeFirstHalf']=score[2]
+                temp['awayFirstHalf']=score[3]
+
+
+                match['awayPastMatches'].push(objToSave(temp))
             });
             console.log(match)
 
